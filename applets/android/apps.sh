152998948bdee39e1d2e2a6791441e29a2eecd07
theo <theodeloose@gmail.com>
2022-07-08 02:41:18 +0200

    Added rofi

---
+++ applets/android/apps.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+dir="$HOME/.config/rofi/applets/android"
+rofi_command="rofi -theme $dir/six.rasi"
+
+# Links
+terminal=""
+files=""
+editor=""
+browser=""
+music=""
+settings=""
+
+# Error msg
+msg() {
+	rofi -theme "$dir/message.rasi" -e "$1"
+}
+
+# Variable passed to rofi
+options="$terminal\n$files\n$editor\n$browser\n$music\n$settings"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Most Used" -dmenu -selected-row 0)"
+case $chosen in
+    $terminal)
+        	alacritty &
+	;;
+    $files)
+		alacritty -e ranger &
+        ;;
+    $editor)
+		if [[ -f /usr/bin/geany ]]; then
+			geany &
+		elif [[ -f /usr/bin/leafpad ]]; then
+			leafpad &
+		elif [[ -f /usr/bin/mousepad ]]; then
+			mousepad &
+		elif [[ -f /usr/bin/code ]]; then
+			code &
+		else
+			msg "No suitable text editor found!"
+		fi
+        ;;
+    $browser)
+		if [[ -f /usr/bin/firefox ]]; then
+			firefox &
+		elif [[ -f /usr/bin/chromium ]]; then
+			chromium &
+		elif [[ -f /usr/bin/midori ]]; then
+			midori &
+		else
+			msg "No suitable web browser found!"
+		fi
+        ;;
+    $music)
+		if [[ -f /usr/bin/lxmusic ]]; then
+			lxmusic &
+		else
+			msg "No suitable music player found!"
+		fi
+        ;;
+    $settings)
+		if [[ -f /usr/bin/xfce4-settings-manager ]]; then
+			xfce4-settings-manager &
+		else
+			msg "No suitable settings manager found!"
+		fi
+        ;;
+esac
+
